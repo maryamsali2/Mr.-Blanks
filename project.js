07a2255294b34917c61872809bfea16807d6f55a
maryamsali02 <maryamsalah233@gmail.com>
2025-07-02 14:50:27 +0300

    string statment

--- project.js
+++ project.js
@@ -1,16 +1,39 @@
 // Variables
 //adding the variables is the thinga that in might change in the game
 
-let hint = 0 //for changing the hint on every level
+let clue = 0 //for changing the hint on every level
 let guessedLetters = [] //it will save the wrong guessed letters if the player add it more than one time
-let level = 1
-let isWinner = false
+let Level = 1
+let isWinner = false 
+let blanks 
 
 // Cached Elements
+//arays
+
+let levels = [
+    {
+        word : "Book",
+        clue : "I have no voice, yet I carry knowledge 🤫.  I have no mind, yet I make yours grow. 🧠You turn me inside out, yet I do not feel. 🔄I dress in colors, but I have no skin. 🎨 You seek me in silence, and I always answer. 🌌Who am I?"
+    },
+    {
+        word : "Keyboard",
+        clue : " 🗝 I have keys but open no locks.🚀 I have space but no room.🚪 You can enter, but I’m not a door.🤔 What am I?"
+    }
+]// im going to add more arrays but just two clues so ill know how every thing is going to be when it start working 
 
 
-// Functions
+// down here is the how the clue will be shown withoout the answers 
+const hintEl = document.querySelector("#hint")
+let level = 0;
+hintEl.textContent = levels[level].clue
+
+// in this step were adding the blanks 
+//string statment
+
+//for the 1st clue
+ const string = "Book"
+ const nm1 = string.split("")
+ console.log(nm1);
 
 
-// Event Listeners
 
