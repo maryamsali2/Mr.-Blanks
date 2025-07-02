620e7285729a85e38b3b455abfe5bcc8d05c77f6
maryamsali02 <maryamsalah233@gmail.com>
2025-07-02 10:32:53 +0300

    starting with the js

--- project.js
+++ project.js
@@ -1,5 +1,10 @@
 // Variables
+//adding the variables is the thinga that in might change in the game
 
+let hint = 0 //for changing the hint on every level
+let guessedLetters = [] //it will save the wrong guessed letters if the player add it more than one time
+let level = 1
+let isWinner = false
 
 // Cached Elements
 
