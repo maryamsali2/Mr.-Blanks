a049ba20f9328c557ad071111eab2bef7afc164d
maryamsali02 <maryamsalah233@gmail.com>
2025-07-03 15:52:03 +0300

    completed game functionality

--- project.js
+++ project.js
@@ -5,14 +5,14 @@ let clue = 0 //for changing the hint on every level
 let wrongLetters = [] //it will save the wrong guessed letters if the player add it more than one time
 let correctLetters = []
 let level = 0
-let beingPlayed = false
+let winner = false
 let blanks
 let chosenWord
 let splitWord
 
 let levels = [
     {
-        word : "Car",
+        word : "CAR",
         clue : "I have no voice, yet I carry knowledge 🤫.  I have no mind, yet I make yours grow. 🧠You turn me inside out, yet I do not feel. 🔄I dress in colors, but I have no skin. 🎨 You seek me in silence, and I always answer. 🌌Who am I?"
     },
     {
@@ -45,12 +45,23 @@ const createSpans = () => {
 
 const matchLetter = () => {
     // for loop through all of the letters in chosenWord to check if the event.target.innerText matches the letter
-    if (event.target.innerText === true) {
-        updateDisplay()
-        checkAnswer()
+    if (splitWord.includes(event.target.innerText)) {
+        if (correctLetters.includes(event.target.innerText)){
+            return
+        }else{
+            correctLetters.push(event.target.innerText)
+        }    
+        console.log("correct letters", correctLetters)
     } else {
-
+        if (wrongLetters.includes(event.target.innerText)) {
+            return
+        } else {
+            wrongLetters.push(event.target.innerText)
+        }
+        console.log("wrong letters:", wrongLetters)
     }
+    updateDisplay()
+    checkAnswer()
 } 
 
 const updateDisplay = () => {
@@ -58,8 +69,25 @@ const updateDisplay = () => {
 }
 
 const checkAnswer = () => {
-    // if all the letters in chosenWord are in the correctLetters array
-    // Then player wins!d
+    let answer = new Array(correctLetters)
+    let solution = new Array(splitWord)
+    // sort both answer and solution arrays the soloution to do an arrays --    Array.prototype.sort()
+    console.log(answer)
+    console.log(solution)
+    // if answer === solution, player wins
+    
+    if(JSON.stringify(answer) === JSON.stringify(solution)) {
+        winner = true
+        console.log(winner)
+        gameEnd()
+    }else{
+        return
+    }
+    
+}
+
+const gameEnd = () => {
+
 }
 
 // Event Listeners
@@ -69,8 +97,8 @@ let buttons=document.querySelectorAll('button')
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
         matchLetter(event.target.innerText)
-        console.log(event.target.innerText)
-        console.log(chosenWord)
+        // console.log(event.target.innerText)
+        // console.log(chosenWord)
     })
 })
 
@@ -79,6 +107,7 @@ const initializeGame = () => {
     hintEl.textContent = levels[level].clue
     chooseWord()
     createSpans()
+    console.log(splitWord)
 }
 
 initializeGame()
